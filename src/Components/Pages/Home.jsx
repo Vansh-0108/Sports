b08719629c3ebc4a8d80e8a53cbc4e2c348d4e53
Vansh-0108 <vanshmahajan2001@gmail.com>
2024-02-27 17:45:36 +0530

    Change bg image source

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -7,7 +7,7 @@ import NewLaunches from '../NewLaunches'
 function Home() {
   return (
     <>
-        <div className="flex min-h-screen items-center bg-cover bg-[url('./src/assets/Bg.jpg')] align-center overflow-hidden">
+        <div className="flex min-h-screen items-center bg-cover bg-[url('https://img.freepik.com/free-photo/front-view-male-rugby-player-holding-ball-hip-with-copy-space_23-2148793355.jpg?w=996&t=st=1709036061~exp=1709036661~hmac=e196bdfbe03e84a2b3f1539a6dca2a2cfbb0f3b30f924608ff449624aa06b12f')] align-center overflow-hidden">
             {/* <img src={bgImage} alt="" /> */}
             <div className='text-white text-4xl w-[50%] p-4'>
                 <h1 className='capitalize'>Get the best of all the brands </h1>
